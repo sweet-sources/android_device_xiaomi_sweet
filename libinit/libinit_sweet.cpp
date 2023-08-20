066c36ccd1ff2ac312a8e29b1e4efd7d67da1434
zaidkhan0997 <kzaid0997@gmail.com>
2023-08-20 20:09:26 +0530

    sweet: Do not set stock build fingerprint
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- libinit/libinit_sweet.cpp
+++ libinit/libinit_sweet.cpp
@@ -17,7 +17,6 @@ static const variant_info_t sweetin_info = {
     .device = "sweetin",
     .marketname = "Redmi Note 10 Pro",
     .model = "M2101K6P",
-    .build_fingerprint = "Redmi/sweetin/sweetin:13/RKQ1.210614.002/V14.0.1.0.TKFINXM:user/release-keys",
     .build_description = "sweetin-user 13 TKQ1.221013.002 V14.0.1.0.TKFINXM release-keys",
 
     .nfc = false,
@@ -31,7 +30,6 @@ static const variant_info_t sweetinpro_info = {
     .device = "sweetin",
     .marketname = "Redmi Note 10 Pro Max",
     .model = "M2101K6I",
-    .build_fingerprint = "Redmi/sweetinpro/sweetin:13/RKQ1.210614.002/V14.0.1.0.TKFINXM:user/release-keys",
     .build_description = "sweetinpro-user 13 TKQ1.221013.002 V14.0.1.0.TKFINXM release-keys",
 
     .nfc = false,
@@ -45,7 +43,6 @@ static const variant_info_t sweet_global_info = {
     .device = "sweet",
     .marketname = "Redmi Note 10 Pro",
     .model = "M2101K6G",
-    .build_fingerprint = "Redmi/sweet_global/sweet:13/RKQ1.210614.002/V14.0.1.0.TKFMIXM:user/release-keys",
     .build_description = "sweet-user 13 TKQ1.221013.002 V14.0.1.0.TKFMIXM release-keys",
 
     .nfc = true,

33433c9e21a2f3b237784e3688d95aff0f10ac54
zaidkhan0997 <kzaid0997@gmail.com>
2023-08-20 19:55:55 +0530

    sweet: Bringup Evolution-X OFFICIAL
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/evolution_sweet.mk
+
+COMMON_LUNCH_CHOICES := \
+    evolution_sweet-user \
+    evolution_sweet-userdebug \
+    evolution_sweet-eng
--- evolution_sweet.mk
+++ evolution_sweet.mk
@@ -11,10 +11,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Evolution-X stuff
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Evolution-X Stuff
+EVO_BUILD_TYPE := OFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Device Identifier.
+PRODUCT_NAME := evolution_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
